2647a6510db0325467f5a05c78bfa14f39779356
yunxqin <1574783270@qq.com>
2024-12-21 17:08:29 +0800

    okok

---
+++ 4.py
@@ -0,0 +1,18 @@
+'''给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数 。
+算法的时间复杂度应该为 O(log (m+n))'''
+
+from typing import List
+
+nums1 = [1,2] 
+nums2 = [3,4]
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+
+        pass
+
+
+a=Solution()
+a.findMedianSortedArrays(nums1,nums2)
